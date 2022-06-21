c159e8415d8f4767d1df086765dd3b4c40fcbba1
DY-2001 <89105143+DY-2001@users.noreply.github.com>
2022-06-21 17:41:21 +0530

    Create Display longest name.cpp
    link : https://practice.geeksforgeeks.org/problems/display-longest-name0853/1/?page=1&difficulty[]=-2&category[]=Arrays&sortBy=submissions

---
+++ Array/Display longest name.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        int max = 0;
+        int index;
+        for(int i = 0; i < n; i++) {
+            if(names[i].size() > max) {
+                max = names[i].size();
+                index = i;
+            }
+        }
+        return names[index];
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+  // } Driver Code Ends
