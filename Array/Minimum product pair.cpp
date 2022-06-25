9b4bcd2d23f75c59cecad304d60b22f3b6e79414
DY-2001 <89105143+DY-2001@users.noreply.github.com>
2022-06-26 01:29:41 +0530

    Create Minimum product pair.cpp
    link : https://practice.geeksforgeeks.org/problems/minimum-product-pair3608/1/?page=5&difficulty[]=-1&category[]=Arrays&sortBy=submissions#

---
+++ Array/Minimum product pair.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+class Solution{
+  public:
+    long long int printMinimumProduct(int arr[], int n) {
+        sort(arr, arr + n);
+        long long int pro = 1;
+        pro = pro * (long long int)arr[0];
+        pro = pro * (long long int)arr[1];
+        return pro;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int i, a[n];
+        for (i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        cout << obj.printMinimumProduct(a, n) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
