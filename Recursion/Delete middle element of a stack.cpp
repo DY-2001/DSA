d163687dadda4d2d0d6ded56b094e6100c938f65
DY-2001 <89105143+DY-2001@users.noreply.github.com>
2022-07-16 02:29:55 +0530

    Create Delete middle element of a stack.cpp
    link : https://practice.geeksforgeeks.org/problems/delete-middle-element-of-a-stack/1/?page=1&difficulty[]=0&category[]=Recursion&sortBy=submissions#

---
+++ Recursion/Delete middle element of a stack.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        int position = sizeOfStack;
+        delete_ele(s, position, sizeOfStack);
+    }
+    
+    void delete_ele(stack<int>&s, int position, int sizeOfStack) {
+        if(position == (sizeOfStack + 1) / 2) {
+            s.pop();
+            return;
+        }
+        
+        int value = s.top();
+        s.pop();
+        delete_ele(s, position - 1, sizeOfStack);
+        s.push(value);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+  // } Driver Code Ends
